01fc592102fe3be8ebf2c9c2d4a0a1429223e5a5
thatsrux <vincenzom121@gmail.com>
2025-07-06 17:47:43 +0200

    losses_plot

--- Trainer.py
+++ Trainer.py
@@ -7,7 +7,7 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from ImageProcessor import ImageProcessor
 
 class Trainer:
-    def __init__(self, model, train_loader, val_loader, criterion, optimizer, device, early_stopping_patience=20):
+    def __init__(self, model, train_loader, val_loader, criterion, optimizer, device, focal_loss, dice_loss, early_stopping_patience=20):
         self.model = model
         self.train_loader = train_loader
         self.val_loader = val_loader
@@ -17,10 +17,14 @@ class Trainer:
         self.image_processor = ImageProcessor()
         self.early_stopping_patience = early_stopping_patience
         self.num_labels = 9  # classi da 0 a 8 (0 = background)
+        self.focal_loss = focal_loss
+        self.dice_loss = dice_loss
 
     def train_epoch(self):
         self.model.train()
         running_loss = 0.0
+        running_focal = 0.0
+        running_dice = 0.0
         with tqdm(self.train_loader, desc="Training", leave=True) as pbar:
             for batch_idx, (images, labels) in enumerate(pbar):
                 images = images.to(self.device)
@@ -30,16 +34,25 @@ class Trainer:
                 if isinstance(outputs, dict):
                     outputs = outputs["out"]
                 loss = self.criterion(outputs, labels)
+                focal = self.focal_loss(outputs, labels).item()
+                dice = self.dice_loss(outputs, labels).item()
                 loss.backward()
                 self.optimizer.step()
                 running_loss += loss.item()
+                running_focal += focal
+                running_dice += dice
                 # Aggiorna la barra con la loss media corrente
                 pbar.set_postfix({"loss": f"{loss.item():.4f}"})
-        return running_loss / len(self.train_loader)
+        avg_loss = running_loss / len(self.train_loader)
+        avg_focal = running_focal / len(self.train_loader)
+        avg_dice = running_dice / len(self.train_loader)
+        return avg_loss, avg_focal, avg_dice
 
     def validate_epoch(self):
         self.model.eval()
         total_loss = 0.0
+        total_focal = 0.0
+        total_dice = 0.0
         all_predictions = []
         all_labels = []
         with torch.no_grad():
@@ -51,13 +64,19 @@ class Trainer:
                     if isinstance(outputs, dict):
                         outputs = outputs["out"]
                     loss = self.criterion(outputs, labels)
+                    focal = self.focal_loss(outputs, labels).item()
+                    dice = self.dice_loss(outputs, labels).item()
                     total_loss += loss.item()
+                    total_focal += focal
+                    total_dice += dice
                     predictions = self.image_processor.postprocess(outputs)
                     all_predictions.append(predictions.cpu())
                     all_labels.append(labels.cpu())
                     # Aggiorna la barra con la loss media corrente
                     pbar.set_postfix({"loss": f"{loss.item():.4f}"})
         avg_loss = total_loss / len(self.val_loader) if len(self.val_loader) > 0 else 0.0
+        avg_focal = total_focal / len(self.val_loader) if len(self.val_loader) > 0 else 0.0
+        avg_dice = total_dice / len(self.val_loader) if len(self.val_loader) > 0 else 0.0
         avg_iou = 0.0
         if len(all_predictions) > 0 and len(all_labels) > 0:
             concatenated_predictions = torch.cat(all_predictions).numpy()
@@ -68,15 +87,19 @@ class Trainer:
             avg_iou = np.nanmean(iou_scores_all_classes)
         else:
             avg_ious_per_class = np.zeros(self.num_labels)
-        print(f"mean val IoU by class: {[f'{iou:.4f}' for iou in avg_ious_per_class]}")
+        print(f"Mean IoU by class: {[f'{iou:.4f}' for iou in avg_ious_per_class]}")
 
-        return avg_iou, avg_loss
+        return avg_iou, avg_loss, avg_focal, avg_dice
 
     def run(self, num_epochs, model_save_path):
         best_val_iou = float('-inf')
         epochs_no_improve = 0
         train_losses = []
         val_losses = []
+        train_focal_losses = []
+        val_focal_losses = []
+        train_dice_losses = []
+        val_dice_losses = []
 
         # Inizializza lo scheduler
         scheduler = ReduceLROnPlateau(
@@ -89,12 +112,16 @@ class Trainer:
         for epoch in range(num_epochs):
             print(f"\nEpoch {epoch + 1}/{num_epochs}")
 
-            train_loss = self.train_epoch()
+            train_loss, train_focal, train_dice = self.train_epoch()
             train_losses.append(train_loss)
+            train_focal_losses.append(train_focal)
+            train_dice_losses.append(train_dice)
             print(f"Train Loss: {train_loss:.4f}")
 
-            val_iou, val_loss = self.validate_epoch()
+            val_iou, val_loss, val_focal, val_dice = self.validate_epoch()
             val_losses.append(val_loss)
+            val_focal_losses.append(val_focal)
+            val_dice_losses.append(val_dice)
             print(f"Validation mIoU: {val_iou:.4f}, Validation Loss: {val_loss:.4f}")
 
             # Step del scheduler sulla validation loss
@@ -103,18 +130,18 @@ class Trainer:
             if val_iou > best_val_iou:
                 best_val_iou = val_iou
                 torch.save(self.model.state_dict(), model_save_path)
-                print(f"Nuovo modello migliore salvato in {model_save_path}")
+                print(f"New best model saved in {model_save_path}")
                 epochs_no_improve = 0
             else:
                 epochs_no_improve += 1
-                print(f"Nessun miglioramento per {epochs_no_improve} epoche.")
+                print(f"No improvement for {epochs_no_improve} epochs.")
 
             if epochs_no_improve >= self.early_stopping_patience:
-                print(f"Early stopping attivato dopo {epoch + 1} epoche. Miglior mIoU: {best_val_iou:.4f}")
+                print(f"Early stopping activated after {epoch + 1} epochs. Best mIoU: {best_val_iou:.4f}")
                 break
 
-        print(f"Modello migliore (mIoU={best_val_iou:.4f}) salvato in {model_save_path}")
-        self.plot_losses(train_losses, val_losses)
+        print(f"Best model (mIoU={best_val_iou:.4f}) saved in {model_save_path}")
+        self.plot_losses(train_losses, val_losses, train_dice_losses, val_dice_losses, train_focal_losses, val_focal_losses)
 
 
     @staticmethod
@@ -169,13 +196,31 @@ class Trainer:
                 iou_scores[label] = intersection / union
         return iou_scores
 
-    def plot_losses(self, train_losses, val_losses):
-        plt.figure(figsize=(8, 5))
-        plt.plot(train_losses, label='Training Loss')
-        plt.plot(val_losses, label='Validation Loss')
-        plt.xlabel('Epoch')
-        plt.ylabel('Loss')
-        plt.title('Training vs Validation Loss')
-        plt.legend()
-        plt.grid(True)
+    def plot_losses(self, train_losses, val_losses, train_dice_losses, val_dice_losses, train_focal_losses, val_focal_losses):
+        fig, axs = plt.subplots(3, 1, figsize=(10, 15))
+        # Plot Loss
+        axs[0].plot(train_losses, label='Training Loss')
+        axs[0].plot(val_losses, label='Validation Loss')
+        axs[0].set_xlabel('Epoch')
+        axs[0].set_ylabel('Loss')
+        axs[0].set_title('Training vs Validation Loss')
+        axs[0].legend()
+        axs[0].grid(True)
+        # Plot Dice Loss
+        axs[1].plot(train_dice_losses, label='Train Dice Loss')
+        axs[1].plot(val_dice_losses, label='Validation Dice Loss')
+        axs[1].set_xlabel('Epoch')
+        axs[1].set_ylabel('Dice Loss')
+        axs[1].set_title('Train vs Validation Dice Loss')
+        axs[1].legend()
+        axs[1].grid(True)
+        # Plot Focal Loss
+        axs[2].plot(train_focal_losses, label='Train Focal Loss')
+        axs[2].plot(val_focal_losses, label='Validation Focal Loss')
+        axs[2].set_xlabel('Epoch')
+        axs[2].set_ylabel('Focal Loss')
+        axs[2].set_title('Train vs Validation Focal Loss')
+        axs[2].legend()
+        axs[2].grid(True)
+        plt.tight_layout()
         plt.show()
